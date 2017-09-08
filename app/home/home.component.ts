28d1f12848c54f18ce02d54ba1343cae2a3aa9b2
Mohit Patil <mohitpatil546@gmail.com>
2017-09-08 17:22:07 +0530

    home component added

---
+++ app/home/home.component.ts
@@ -0,0 +1,42 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+import { User } from '../_models/index';
+import { AlertService, UserService } from '../_services/index';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'home.component.html'
+})
+
+export class HomeComponent implements OnInit {
+    currentUser: User;
+    users: User[] = [];
+
+    constructor(
+        private userService: UserService,
+        private alertService: AlertService) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+
+    ngOnInit() {
+        this.loadAllUsers();
+        this.loadAllRentals();
+    }
+
+    deleteUser(id: number) {
+        this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
+    }
+    
+    deleteRental(id: number) {
+        this.userService.deleteRental(id).subscribe(() => { this.loadAllRentals() });
+    }
+
+    private loadAllUsers() {
+        this.userService.getAll().subscribe(users => { this.users = users; });
+    }
+
+    private loadAllRentals() {
+        this.userService.getAllRentals().subscribe(rentals => { this.rentals = rentals; });
+    }
+    
+}
