c51e0cfe763f36d87447fb7a162b95222d1a93fd
Mohit Patil <mohitpatil546@gmail.com>
2017-09-08 17:20:37 +0530

    user service added

---
+++ app/_services/user.service.ts
@@ -0,0 +1,52 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+
+import { User, Rental } from '../_models/index';
+
+@Injectable()
+export class UserService {
+    constructor(private http: Http) { }
+
+    getAll() {
+        return this.http.get('/api/users', this.jwt()).map((response: Response) => response.json());
+    }
+
+    getAllRentals() {
+        return this.http.get('/api/rental', this.jwt()).map((response: Response) => response.json());
+    }
+
+    getById(id: number) {
+        return this.http.get('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+    }
+
+    create(user: User) {
+        return this.http.post('/api/users', user, this.jwt()).map((response: Response) => response.json());
+    }
+
+    update(user: User) {
+        return this.http.put('/api/users/' + user.id, user, this.jwt()).map((response: Response) => response.json());
+    }
+
+    delete(id: number) {
+        return this.http.delete('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+    }
+
+    deleteRental(id: number) {
+        return this.http.delete('/api/rental/' + id, this.jwt()).map((response: Response) => response.json());
+    }
+
+    addRental(rental: Rental) {
+        return this.http.post('/api/rental', rental, this.jwt()).map((response: Response) => response.json());
+    }
+
+    // private helper methods
+
+    private jwt() {
+        // create authorization header with jwt token
+        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        if (currentUser && currentUser.token) {
+            let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token });
+            return new RequestOptions({ headers: headers });
+        }
+    }
+}
